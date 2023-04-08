26d3f90d2e3c5035c078919659b76b5f4e7db20e
Omwitsa <omwitsawilson@yahoo.com>
2023-04-08 17:51:08 +0300

    Login Screen

--- app/src/main/java/com/example/easysaccoapp/MainActivity.java
+++ app/src/main/java/com/example/easysaccoapp/MainActivity.java
@@ -1,14 +1,32 @@
 package com.example.easysaccoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_login;
+    EditText et_username, et_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_login = (Button) findViewById(R.id.login);
+        et_username = (EditText) findViewById(R.id.username);
+        et_password = (EditText) findViewById(R.id.password);
+
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
